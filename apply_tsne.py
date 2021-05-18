3a7481d5ea8b752b1f02b29f9a08837e150ce62b
gsaygili <gsaygili@gmail.com>
2021-05-18 20:17:13 +0200

    Updated README.md

--- apply_tsne.py
+++ apply_tsne.py
@@ -12,21 +12,32 @@ def tsne_p(data, dim=2, perplexity=30):
     return X_embedded
 
 
+def plot_embedding(X_d, y):
+    labels = str(y).strip('[]')
+    plt.figure()
+    colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'olive', 'orange', 'purple'
+    for i, c in zip(y, colors):
+        plt.scatter(X_d[y == i, 0], X_d[y == i, 1], c=c, s=3)
+    # plt.scatter(X_d[15, 0], X_d[15, 1], color="none", edgecolor="red")
+    plt.show()
+
+
 def apply_tsne_subsets(dim=2, perplexity=30, size=5000, mnist_folder="/home/gorkem/datasets/"):
     _, _, save_path = mnist.mnist_subsets(mnist_folder=mnist_folder, size=size)
     # find number of subsets
     X_files = sorted(glob.glob(save_path+"X*.npy"))
     for i in range(len(X_files)):
         X = np.load(X_files[i])
-        X_emb = tsne_p(X, dim=2, perplexity=perplexity)
+        X_emb = tsne_p(X, dim=dim, perplexity=perplexity)
         # create an embedding subfolder and save with perplexity info
         Path(save_path+"emb_p"+str(perplexity)+"/").mkdir(parents=True, exist_ok=True)
         np.save(save_path+"emb_p"+str(perplexity)+"/"+"Xemb_"+str(i), X_emb)
 
 
-# plt.figure(figsize=(6, 5))
-# colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
-# for i, c in zip(y, colors):
-#     plt.scatter(X_embedded[y == i, 0], X_embedded[y == i, 1], c=c)
-# plt.show()
-apply_tsne_subsets()
+emb_folder = "/home/gorkem/datasets/mnist_subsets/5000/emb_p30/"
+y_folder = "/home/gorkem/datasets/mnist_subsets/5000/"
+Xe = np.load(emb_folder+"Xemb_0.npy")
+y = np.load(y_folder+"y_5000_0.npy")
+plot_embedding(Xe, y)
+
+
