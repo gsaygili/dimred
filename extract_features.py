3d4deb6418e581fb23d7febdea8282dd0e164f4f
gsaygili <gsaygili@gmail.com>
2021-05-20 00:03:00 +0200

    Error functions are created There is a problem in marking errors on the scatter plot - The problem solved -> scatter plots with errors are correct! - Five feature extraction completed

--- extract_features.py
+++ extract_features.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import calc_error as err
 import scipy.spatial.distance as dist
-
+import time
 
 emb_folder = "/home/gorkem/datasets/mnist_subsets/5000/emb_p30/"
 y_folder = "/home/gorkem/datasets/mnist_subsets/5000/"
@@ -11,21 +11,26 @@ y = np.load(y_folder+"y_5000_0.npy")
 X = np.load(y_folder+"X_5000_0.npy")
 X = X.reshape((X.shape[0], X.shape[1]*X.shape[2]))
 
-# calculate euclidean distances in D
 K = 20
 features = np.zeros((X.shape[0], K, 5))
-X_D = dist.squareform(dist.pdist(X, "euclidean"))
 X_d = dist.squareform(dist.pdist(Xe, "euclidean"))
-sort_D = np.sort(X_D)
 sort_index_d = np.argsort(X_d)
 
+# calculate euclidean distance
+print("Calculating Euclidean Distances")
+start_time = time.time()
+X_D = dist.squareform(dist.pdist(X, "euclidean"))
+sort_D = np.sort(X_D)
 for i in range(X_D.shape[0]):
     cost = X_D[i, :]
     s_D = sort_D[i, 1:K+1]
     s_d = cost[sort_index_d[i, 1:K+1]]
     features[i, :, 0] = np.abs(s_D-s_d)
+print("--- Euclidean takes: %s seconds ---" % (time.time() - start_time))
 
 # calculate cosine distance
+print("Calculating Cosine Distances")
+start_time = time.time()
 X_D = dist.squareform(dist.pdist(X, "cosine"))
 sort_D = np.sort(X_D)
 for i in range(X_D.shape[0]):
@@ -33,5 +38,42 @@ for i in range(X_D.shape[0]):
     s_D = sort_D[i, 1:K+1]
     s_d = cost[sort_index_d[i, 1:K+1]]
     features[i, :, 1] = np.abs(s_D-s_d)
+print("--- Cosine takes: %s seconds ---" % (time.time() - start_time))
 
 # calculate minkowski distance 1 (mahalonabis)
+print("Calculating standardized Euclidean distance Distances")
+start_time = time.time()
+X_D = dist.squareform(dist.pdist(X, 'seuclidean', V=None))
+# X_D = dist.squareform(dist.pdist(X, "minkowski", p=1))
+sort_D = np.sort(X_D)
+for i in range(X_D.shape[0]):
+    cost = X_D[i, :]
+    s_D = sort_D[i, 1:K+1]
+    s_d = cost[sort_index_d[i, 1:K+1]]
+    features[i, :, 2] = np.abs(s_D-s_d)
+print("--- standardized Euclidean distance takes: %s seconds ---" % (time.time() - start_time))
+
+# calculate minkowski distance 3
+print("Calculating correlation Distances")
+start_time = time.time()
+# X_D = dist.squareform(dist.pdist(X, "minkowski", p=3))
+X_D = dist.squareform(dist.pdist(X, 'correlation'))
+sort_D = np.sort(X_D)
+for i in range(X_D.shape[0]):
+    cost = X_D[i, :]
+    s_D = sort_D[i, 1:K+1]
+    s_d = cost[sort_index_d[i, 1:K+1]]
+    features[i, :, 3] = np.abs(s_D-s_d)
+print("--- correlation takes: %s seconds ---" % (time.time() - start_time))
+
+# calculate chebyshev distance
+print("Calculating Chebyshev Distances")
+start_time = time.time()
+X_D = dist.squareform(dist.pdist(X, "chebyshev"))
+sort_D = np.sort(X_D)
+for i in range(X_D.shape[0]):
+    cost = X_D[i, :]
+    s_D = sort_D[i, 1:K+1]
+    s_d = cost[sort_index_d[i, 1:K+1]]
+    features[i, :, 4] = np.abs(s_D-s_d)
+print("--- Chebyshev takes: %s seconds ---" % (time.time() - start_time))
