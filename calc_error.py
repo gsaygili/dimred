abd4c4399249f6a918d34c5bfe35d02e47dbcf9e
gsaygili <gsaygili@gmail.com>
2021-08-06 15:48:28 +0200

    list of worst N and best N neighbors in calc_error.py

--- calc_error.py
+++ calc_error.py
@@ -1,7 +1,5 @@
 import numpy as np
-# import apply_tsne as at
 import scipy.spatial.distance as dist
-# from matplotlib import pyplot as plt
 
 
 def find_errors_majority(X_emb, labels, K=20):
@@ -34,15 +32,19 @@ def find_errors_nearest(X_emb, labels):
     return err
 
 
-# # plot errors
-# emb_folder = "/home/gorkem/datasets/mnist_subsets/5000/emb_p30/"
-# y_folder = "/home/gorkem/datasets/mnist_subsets/5000/"
-# Xe = np.load(emb_folder+"Xemb_0.npy")
-# y = np.load(y_folder+"y_5000_0.npy")
-# errs_m = find_errors_majority(Xe, y)
-# at.plot_embedding(Xe, y)
-# plt.figure(1)
-# at.plot_embedding_with_errors(Xe, y, errs_m)
-# errs_n = find_errors_nearest(Xe, y)
-# at.plot_embedding_with_errors(Xe, y, errs_n)
-# plt.figure(2)
+def find_worst_best_N(X_emb, labels, K=20, N=10):
+    # take a closest neighborhood of K and check whether the label of the majority is the same as the center
+    X_d = dist.squareform(dist.pdist(X_emb, "euclidean"))
+    corr_neigh_num = np.zeros(X_emb.shape[0])
+    # the first column is the sample itself since the distance is zero
+    sort_index = np.argsort(X_d)
+    error_list = []
+    for i in range(X_d.shape[0]):
+        K_neigh = sort_index[i, 1:K+1]
+        lab_neigh = labels[K_neigh]
+        num_label = np.count_nonzero(lab_neigh == labels[i])  # dogru komsu sayisi
+        corr_neigh_num[i] = num_label
+    bestN = np.argsort(corr_neigh_num)[-N:]
+    worstN = np.argsort(corr_neigh_num)[:N]
+    return worstN, bestN
+
